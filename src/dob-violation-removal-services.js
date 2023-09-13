fe1ffa1c11c76a3b89d25783e9a839e995dff7ad
Dromance <88290916+pflaquer@users.noreply.github.com>
2023-09-13 17:14:08 -0400

    Create dob-violation-removal-services.js

---
+++ src/dob-violation-removal-services.js
@@ -0,0 +1,43 @@
+function removalServices(){
+  return (`
+
+  <header class="jumbotron my-4">
+        <h1 class="display-4" style="filter: drop-shadow(4px 3px 5px gray);">The Ultimate Guide to Removing a Building Violation in New York City</h1>
+        <p><img src="https://a810-bisweb.nyc.gov/bisweb/images/doblogo_1.jpg" style="border-radius:25px;"</p>
+        <section>
+      <p class="lead">Ignoring building violations can have serious consequences. The DOB can issue fines, order the property to be vacated, or even demolish the property. In addition, property owners with outstanding violations may have difficulty obtaining financing or selling their property.
+    Building violations can be a headache for property owners. Not only can they lead to fines, but they can also make it difficult to sell or refinance your property. If you're facing building violations, there are a few things you can do to get them removed.</p>
+        
+        </section>
+    <section>
+      <h2 class="display-4">How to Remove a Building Violation</h2>
+      <p class="lead">There are a few ways to remove a building violation. You can:</p>
+      <ul>
+        <li class="lead">Correct the violation and submit proof to the DOB.</li>
+        <li class="lead">Request a hearing with the DOB to contest the violation.</li>
+        <li class="lead">Hire a <a href="https://www.flaquer.net">violation removal services company</a> to help you get the violation dismissed.</li>
+      </ul>
+
+      <p class="lead">The most obvious way to remove a building violation is to correct the problem that caused it. This may involve making repairs, removing hazardous materials, or updating your electrical system. Once the violation has been corrected, you can submit proof of compliance to the building department.
+      <p class="lead">Removing a building violation can be a daunting task, but it's important to remember that you have options. By following these tips, you can increase your chances of getting the violation removed and avoiding any further problems.
+      </p>
+    </section>
+    <section>
+      <h2 class="display-4">Why Choose Flaquer Design Group for Violation Removal Services?</h2>
+      <p class="lead">If you're facing building violations, hiring a violation removal services company can be a great way to get the violations dismissed quickly and easily. Violation removal companies have the experience and expertise to navigate the DOB process and get the results you need.</p>
+      <p class="lead">In addition, violation removal companies can often get violations dismissed even if you're guilty. This is because they know how to build a strong case and present it to the DOB in a way that is likely to succeed.</p>
+    </section>
+    <section>
+      <h2 class="display-4">Contact Us Today</h2>
+      <p class="lead">If you're facing building violations in NYC, don't wait any longer. Contact us today to learn more about how our violation removal services can help you get your violations dismissed.</p>
+      <p class="lead">
+        <a href="tel:" class="btn btn-warning btn-lg"><i class="material-icons" style="font-size:30px;color:orangered">phone_in_talk</i>Call: (212) DESIGN</a><br><a href="#" target="" class="btn btn-warning"><i class="material-icons">email</i>Get a Free Quote</a>
+
+      </p>
+    </section>
+
+          </header>
+          `)
+}
+
+export 
