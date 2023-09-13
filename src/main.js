e8be25f6253b134f8e6285e5e4b10b5d6f3d685a
Dromance <88290916+pflaquer@users.noreply.github.com>
2023-09-13 17:34:54 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import index  from './content.js'
 import removalServices from './dob-violation-removal-services.js'
-
-if(document.getElementById("main-container").getAttribute("name") === "index"){
-  document.getElementById("main-container").innerHTML = index()
+let d = document.getElementById("main-container";
+if(d.getAttribute("name") === "index"){
+  d.innerHTML = index()
 }
 
