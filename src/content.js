1d2ea3241eb087d10c017ccabf0636057166948b
Dromance <88290916+pflaquer@users.noreply.github.com>
2023-09-13 17:02:30 -0400

    Create content.js

---
+++ src/content.js
@@ -0,0 +1,89 @@
+function index(){
+return (`
+
+<!-- Page Content -->
+<main role="main">
+      <!-- Jumbotron Header -->
+      <div class="jumbotron my-4" style="background-image:linear-gradient (white,lightyellow);">
+        <div class="container" style="margin-top:75px;">
+        <h1 class="display-4" style="filter: drop-shadow(4px 3px 5px gray);">Engineering, Architectural and Inspection Services</h1>
+        <p class="lead">We are a family run architecture and engineering design firm.  We provide services throughout the New York metropolitan area, NJ and PA and are able to provide the best rates through our unique bidding process.  All initial consultations are free of charge.</p>
+        <p>
+<ul>
+  <li class="lead">&#x2705; Professional Engineers, Architects and Contractors</li>
+  <li class="lead">&#x2705; Quick Turnaround Time</li>
+  <li class="lead">&#x2705; Over 30 Years Experience</li>
+</ul>
+
+          
+        </p>
+          
+          <a href="tel:9179519208" class="btn btn-warning btn-lg" style="padding:5px;">
+          <i class="material-icons" style="font-size:30px;color:orangered">phone_in_talk</i>Call: (917) 951-9208 </a><br>
+          <hr>
+          <p><a href="tel:9179519208" target="" class="btn btn-warning"><i class="material-icons">email</i>Get a Free Quote</a>
+</div>
+          
+      </div>
+</main>
+</div>
+
+      <!-- Page Features -->
+  <div class="container">
+      <div class="row text-center">
+<p id="content">
+  
+
+        <div class="col-lg-4 col-md-6 mb-4">
+          <div class="card" style="height:600px;">
+            <img class="card-img-top" src="https://foyr.com/learn/wp-content/uploads/2019/03/Blueprint.jpg" alt="" style="height:256px;">
+            <div class="card-body">
+              <h4 class="card-title">Architectural Services</h4>
+              <p class="card-text justify-content-left" style="justify-content: left;">We have over 30 years of designing architectural spaces for commercial and residential clients.  We utilize the latest software and rendering engines to provide detailed models and provide a pleasant design experience for our clients.  </p>
+            </div>
+            <div class="card-footer">
+              <a href="tel:9179519208" target="_blank" class="btn btn-warning"><i class="material-icons">email</i>Get a Free Quote</a>
+            </div>
+          </div>
+        </div>
+
+     <div class="col-lg-4 col-md-6 mb-4">
+          <div class="card" style="height:600px;">
+            <img class="card-img-top" src="https://www.chemengonline.com/wp-content/uploads/2018/01/manual-workers-working.jpg" alt="" style="height:256px;">
+            <div class="card-body">
+              <h4 class="card-title">Engineering Services</h4>
+              <p class="card-text justify-content-left">Flaquer Engineering is a leading engineering company that specializes in the design, construction, and maintenance of buildings, roads, bridges, and other infrastructure. With over 30 years of experience, Flaquer Engineering has a proven track record of success in delivering high-quality projects on time and within budget.</p>
+            </div>
+            <div class="card-footer">
+              <a href="tel:9179519208" target="_blank" class="btn btn-warning"><i class="material-icons">email</i>Get a Free Quote</a>
+            </div>
+          </div>
+        </div>
+
+         <div class="col-lg-4 col-md-6 mb-4">
+          <div class="card" style="height:600px;">
+            <img class="card-img-top" src="https://constructionblog.autodesk.com/wp-content/uploads/2020/08/concrete-contractors-in-construction.jpg" alt="" style="height:256px;">
+            <div class="card-body">
+              <h4 class="card-title">Construction Services</h4>
+              <p class="card-text justify-content-left" >We provide construction services for new builds as well as renovations.  From smaller to larger scope builds we are capable and well equipped to handle all kinds of projects.  Residential and Commercial contracting services are available. </p>
+            </div>
+            <div class="card-footer">
+              <a href="tel:9179519208" target="_blank" class="btn btn-warning"><i class="material-icons">email</i>Get a Free Quote</a>
+            </div>
+          </div>
+        </div>
+
+
+        </div>
+
+      </div>
+      <!-- /.row -->
+
+
+    </div>
+    <!-- /.container -->
+
+
+`)
+}
+export default index
