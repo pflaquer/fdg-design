09cf78420bf22515b4e70afa9080df755cb29149
Dromance <88290916+pflaquer@users.noreply.github.com>
2023-09-13 17:35:30 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import index  from './content.js'
 import removalServices from './dob-violation-removal-services.js'
-let d = document.getElementById("main-container";
+let d = document.getElementById("main-container");
+
+//determine which data to render based on page
 if(d.getAttribute("name") === "index"){
   d.innerHTML = index()
 }
