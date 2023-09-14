fca8db2cb34052959c1c30eafed6e0daf1b8dc49
Dromance <88290916+pflaquer@users.noreply.github.com>
2023-09-14 08:16:57 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import index  from './content.js'
 import removalServices from './dob-violation-removal-services.js'
-let d = document.getElementById("main-container");
+import navbar from './navbar.js'
 
+//render navbar
+let n = document.getElementById("nav");
+n.innerHTML = navbar();
+
+let d = document.getElementById("main-container");
+//let page = d.getAttribute('name');
+//d.innerHTML = eval(page+())
 //determine which data to render based on page
 if(d.getAttribute("name") === "index"){
   d.innerHTML = index()
