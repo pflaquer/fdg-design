7208287e52f911dd4d5bdc61256fc865c911a8c6
Dromance <88290916+pflaquer@users.noreply.github.com>
2023-09-14 10:48:36 -0400

    Update cta.js

--- src/cta.js
+++ src/cta.js
@@ -2,12 +2,14 @@ function cta(){
   return (`
 
   <div class="card border-warning">
+  <div style="padding:5px;">
   <h2 class="display-5">Contact Us Today</h2>
       <p class="lead">If you're facing building violations in NYC, don't wait any longer. Contact us today to learn more about how our violation removal services can help you get your violations dismissed.</p>
       <p class="lead">
         <a href="tel:9179519208" class="btn btn-warning btn-lg"><i class="material-icons" style="font-size:30px;color:orangered">phone_in_talk</i>Call: 917-951-9208</a><p><a href="#" target="_blank" class="btn btn-warning"><i class="material-icons">email</i>Get a Free Quote</a></p>
 
       </p>
+      </div>
       </div>
       `)
 }
