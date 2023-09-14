7668519f8dad88aa6ff8293b7b29594007241684
Dromance <88290916+pflaquer@users.noreply.github.com>
2023-09-14 10:26:47 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import index  from './content.js'
 import removalServices from './dob-violation-removal-services.js'
 import navbar from './navbar.js'
 import cta from './cta.js'
+
 //render cta
 
 let cta = document.getElementById("cta");
