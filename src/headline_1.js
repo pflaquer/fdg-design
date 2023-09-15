7a75ef18b461785eb219a3f553df47e1a7380fd3
Dromance <88290916+pflaquer@users.noreply.github.com>
2023-09-15 15:56:52 -0400

    Update headline_1.js

--- src/headline_1.js
+++ src/headline_1.js
@@ -17,7 +17,7 @@ list-style:none;
 <div class="container my-5">
     <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
       <div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
-        <h1 class="display-3 fw-light lh-1 text-body-emphasis bg-warning border-primary " style="margin-top:75px;border-radius:10px;background-image:linear-gradient(lightyellow,white);">
+        <h1 class="display-3 fw-light lh-1 text-body-emphasis bg-warning border-primary " style="margin-top:75px;border-radius:10px;background-image:linear-gradient(lightyellow,white);filter:drop-shadow(5px 3px 1px blue">
  DESIGN + BUILD
  <br>
 </h1>
