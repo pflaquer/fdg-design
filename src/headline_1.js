dc26c52b52f4312d1151ecb27470cc920e344eca
Dromance <88290916+pflaquer@users.noreply.github.com>
2023-09-15 10:30:34 -0400

    Update headline_1.js

--- src/headline_1.js
+++ src/headline_1.js
@@ -24,8 +24,8 @@ list-style:none;
 <div class="container my-5">
     <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
       <div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
-        <h1 class="display-4 fw-bold lh-1 text-body-emphasis" style="margin-top:75px;"><mark>	
- TOP RATED DESIGN + BUILD FIRM</mark>
+        <h1 class="display-4 fw-bold lh-1 text-body-emphasis text-primary" style="margin-top:75px;"><mark>	
+ DESIGN + BUILD FIRM</mark>
  <br>
  &#11088;	
 &#11088;	
