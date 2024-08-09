07ee734781079a93d22aa5832c0e5a0069c8255b
Dromance <88290916+pflaquer@users.noreply.github.com>
2024-08-09 16:23:02 -0400

    Update index_1.js

--- src/index_1.js
+++ src/index_1.js
@@ -20,7 +20,7 @@ function index_1(){
             <img class="card-img-top" src="https://foyr.com/learn/wp-content/uploads/2019/03/Blueprint.jpg" alt="" style="height:256px;">
             <div class="card-body">
               <h4 class="card-title">Architectural Services</h4>
-              <p class="lead" style="justify-content: left;">We have over 30 years of designing architectural spaces for commercial and residential clients.  We utilize the latest software and rendering engines to provide detailed models and provide a pleasant design experience for our clients.  </p>
+              <p class="lead" style="text-align: left;">We have over 30 years of designing architectural spaces for commercial and residential clients.  We utilize the latest software and rendering engines to provide detailed models and provide a pleasant design experience for our clients.  </p>
             </div>
             <div class="card-footer">
               <a href="tel:9179519208" target="_blank" class="btn btn-warning"><i class="material-icons">email</i>Get a Free Quote</a>
@@ -33,7 +33,7 @@ function index_1(){
             <img class="card-img-top" src="https://www.chemengonline.com/wp-content/uploads/2018/01/manual-workers-working.jpg" alt="" style="height:256px;">
             <div class="card-body">
               <h4 class="card-title">Engineering Services</h4>
-              <p class="card-text justify-content-left">Flaquer Design Group is a leading consulting engineering company that specializes in the design, constructio of buildings and infrastructure. With over 30 years of experience, Flaquer Engineering has a proven track record of success in delivering high-quality projects on time and within budget.</p>
+              <p class="lead">Flaquer Design Group is a leading consulting engineering company that specializes in the design, constructio of buildings and infrastructure. With over 30 years of experience, Flaquer Engineering has a proven track record of success in delivering high-quality projects on time and within budget.</p>
             </div>
             <div class="card-footer">
               <a href="tel:9293985614" target="_blank" class="btn btn-warning"><i class="material-icons">email</i>Get a Free Quote</a>
@@ -46,7 +46,7 @@ function index_1(){
             <img class="card-img-top" src="https://constructionblog.autodesk.com/wp-content/uploads/2020/08/concrete-contractors-in-construction.jpg" alt="" style="height:256px;">
             <div class="card-body">
               <h4 class="card-title">Construction Services</h4>
-              <p class="card-text justify-content-left" >We provide construction services for new builds as well as renovations.  From smaller to larger scope builds we are capable and well equipped to handle all kinds of projects.  Residential and Commercial contracting services are available. </p>
+              <p class="lead" >We provide construction services for new builds as well as renovations.  From smaller to larger scope builds we are capable and well equipped to handle all kinds of projects.  Residential and Commercial contracting services are available. </p>
             </div>
             <div class="card-footer">
               <a href="tel:9179519208" target="_blank" class="btn btn-warning"><i class="material-icons">email</i>Get a Free Quote</a>
