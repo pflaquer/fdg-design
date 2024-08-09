a7333227e6c49b76479e0dd40babbfb2a5f4711a
Dromance <88290916+pflaquer@users.noreply.github.com>
2024-08-09 16:27:57 -0400

    Update cta.js

--- src/cta.js
+++ src/cta.js
@@ -1,13 +1,13 @@
 function cta(){
   return (`
 
-  <div class="card border-warning" style="padding:5px;background-image:linear-gradient(white,lightyellow,white);text-align:left;">
+  <div class="card border-warning" style="padding:5px;background-image:linear-gradient(white,lightyellow,white);">
   <div style="padding:7px;">
   <center>
   <h2 class="display-5">Contact Us Today</h2>
-      <p class="lead">If you're facing building violations in NYC, don't wait any longer. Contact us today to learn more about how our violation removal services can help you get your violations dismissed.</p>
+      <p class="lead" style="text-align:left;">If you're facing building violations in NYC, don't wait any longer. Contact us today to learn more about how our violation removal services can help you get your violations dismissed.</p>
       <p class="lead">
-        <a href="tel:9179519208" class="btn btn-warning btn-lg"><i class="material-icons" style="font-size:30px;color:orangered">phone_in_talk</i>Call: 917-951-9208</a><p><a href="#" target="_blank" class="btn btn-warning"><i class="material-icons">email</i>Get a Free Quote</a></p>
+        <a href="tel:9179519208" class="btn btn-warning btn-lg"><i class="material-icons" style="font-size:30px;color:orangered">phone_in_talk</i>Call: 929-398-5614</a><p><a href="#" target="_blank" class="btn btn-warning"><i class="material-icons">email</i>Get a Free Quote</a></p>
 
       </p>
       </center>
