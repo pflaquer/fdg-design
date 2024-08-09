0c4f2c0a5109f4fae238a8d22cc64d3a34fece44
Dromance <88290916+pflaquer@users.noreply.github.com>
2024-08-09 16:35:29 -0400

    Update index_1.js

--- src/index_1.js
+++ src/index_1.js
@@ -28,6 +28,7 @@ function index_1(){
           </div>
         </div>
 
+<!--
      <div class="col-lg-4 col-md-6 mb-4">
           <div class="card" style="height:600px;">
             <img class="card-img-top" src="https://www.chemengonline.com/wp-content/uploads/2018/01/manual-workers-working.jpg" alt="" style="height:256px;">
@@ -40,6 +41,20 @@ function index_1(){
             </div>
           </div>
         </div>
+-->
+<div class="col-lg-4 col-md-6 mb-4">
+          <div class="card" style="height:600px;">
+            <img class="card-img-top" src="https://constructionblog.autodesk.com/wp-content/uploads/2020/08/concrete-contractors-in-construction.jpg" alt="" style="height:256px;">
+            <div class="card-body">
+              <h4 class="card-title">Construction Services</h4>
+              <p class="lead" style="text-align:left;">We provide construction services for new builds as well as renovations.  From smaller to larger scope builds we are capable and well equipped to handle all kinds of projects.  Residential and Commercial contracting services are available. </p>
+            </div>
+            <div class="card-footer">
+              <a href="tel:9293985614" target="_blank" class="btn btn-warning"><i class="material-icons">email</i> Get a Free Quote</a>
+            </div>
+          </div>
+        </div>
+
 
          <div class="col-lg-4 col-md-6 mb-4">
           <div class="card" style="height:600px;">
